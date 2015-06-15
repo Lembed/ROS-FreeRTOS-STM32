f277e0c99242eb6d3c64f5c4f9c28b2dd1690391
yigitgunay <yigit.guenay@rwth-aachen.de>
2015-06-15 16:10:02 +0200

    Refactored code.

--- apps/nodes.h
+++ apps/nodes.h
@@ -1,9 +1,9 @@
 #ifndef ASW_APPS_APPLICATION_TASKS_H_
 #define ASW_APPS_APPLICATION_TASKS_H_
 
-#include "node1/node1.h"
-#include "node2/node2.h"
-#include "ultrasonic_sensor/ultrasonic_sensor.h"
+#include "nodes/node1/node1.h"
+#include "nodes/node2/node2.h"
+#include "nodes/ultrasonic_sensor/ultrasonic_sensor.h"
 
 typedef struct node_descriptor {
 	char name[32];
@@ -12,7 +12,7 @@ typedef struct node_descriptor {
 } node_decriptor;
 
 node_decriptor nodes[] = {
-		//{"ultrasonic_sensor", ultrasonic_sensor},
+		{"ultrasonic_sensor", ultrasonic_sensor},
 		{"node1", node1},
 		//{"node2", node2}
 };
