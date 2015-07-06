6bbc8f3d9b7a7e8b357e86c8ddb4b08920f96737
yigitgunay <yigit.guenay@rwth-aachen.de>
2015-07-06 10:15:47 +0200

    Added debugging code for hard faults. Fixed thread-safety issues in XMLRPCServer.

--- src/stm32/core/hwe/CPU/startup/startup/startup_stm32f4xx.c
+++ src/stm32/core/hwe/CPU/startup/startup/startup_stm32f4xx.c
@@ -141,7 +141,7 @@ extern int main(void);           /*!< The entry point for the application.    */
 //extern void SystemInit(void);    /*!< Setup the microcontroller system(CMSIS) */
 void Default_Reset_Handler(void);   /*!< Default reset handler                */
 static void Default_Handler(void);  /*!< Default exception handler            */
-
+static void DefaultHardFault_Handler(void);
 
 /**
   *@brief The minimal vector table for a Cortex M3.  Note that the proper constructs
@@ -297,7 +297,7 @@ void Default_Reset_Handler(void)
   */
 #pragma weak Reset_Handler = Default_Reset_Handler
 #pragma weak NMI_Handler = Default_Handler
-#pragma weak HardFault_Handler = Default_Handler
+#pragma weak HardFault_Handler = DefaultHardFault_Handler
 #pragma weak MemManage_Handler = Default_Handler
 #pragma weak BusFault_Handler = Default_Handler
 #pragma weak UsageFault_Handler = Default_Handler
@@ -403,5 +403,50 @@ static void Default_Handler(void)
 	{
 	}
 }
+
+void prvGetRegistersFromStack(unsigned long *pulFaultStackAddress )
+{
+/* These are volatile to try and prevent the compiler/linker optimising them
+away as the variables never actually get used.  If the debugger won't show the
+values of the variables, make them global my moving their declaration outside
+of this function. */
+volatile unsigned long r0;
+volatile unsigned long r1;
+volatile unsigned long r2;
+volatile unsigned long r3;
+volatile unsigned long r12;
+volatile unsigned long lr; /* Link register. */
+volatile unsigned long pc; /* Program counter. */
+volatile unsigned long psr;/* Program status register. */
+
+    r0 = pulFaultStackAddress[ 0 ];
+    r1 = pulFaultStackAddress[ 1 ];
+    r2 = pulFaultStackAddress[ 2 ];
+    r3 = pulFaultStackAddress[ 3 ];
+
+    r12 = pulFaultStackAddress[ 4 ];
+    lr = pulFaultStackAddress[ 5 ];
+    pc = pulFaultStackAddress[ 6 ];
+    psr = pulFaultStackAddress[ 7 ];
+
+    /* When the following line is hit, the variables contain the register values. */
+    for( ;; );
+}
+
+static void DefaultHardFault_Handler(void)
+{
+    __asm volatile
+    (
+        " tst lr, #4                                                \n"
+        " ite eq                                                    \n"
+        " mrseq r0, msp                                             \n"
+        " mrsne r0, psp                                             \n"
+        " ldr r1, [r0, #24]                                         \n"
+        " ldr r2, handler2_address_const                            \n"
+        " bx r2                                                     \n"
+        " handler2_address_const: .word prvGetRegistersFromStack    \n"
+    );
+}
+
 
 /*********************** (C) COPYRIGHT 2009 Coocox ************END OF FILE*****/
